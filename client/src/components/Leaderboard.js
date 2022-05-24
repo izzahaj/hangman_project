5b1e96efe455afa107ca1b948e94e5b2acf94b4e
izzahaj <izzah.jalil02@gmail.com>
2022-05-24 16:10:52 +0800

    Added the Leaderboard component

--- client/src/components/Leaderboard.js
+++ client/src/components/Leaderboard.js
@@ -1,8 +1,85 @@
 import React from "react";
 
+const sampleUsers = [
+  {
+    username: "Wen Kai",
+    points: 80200
+  },
+  {
+    username: "Lee Hao Rui",
+    points: 23130
+  },
+  {
+    username: "Bryan Toh",
+    points: 35200
+  },
+  {
+    username: "Nina Gan",
+    points: 21030
+  },
+  { 
+    username: "Benjamin", 
+    points: 12540
+  }, 
+  { 
+    username: "Sharifah", 
+    points: 52590
+  }, 
+  { 
+    username: "Shannon", 
+    points: 42870
+  }, 
+  { 
+    username: "Rachel Goh", 
+    points: 78270,
+  } 
+]
+
+const Profiles = ( {users} ) => {
+  return (
+    <div>{Item(sampleUsers)}</div>
+  )
+}
+
+const Item = (data) => {
+  return (
+    <>
+      {
+        data.map((val, index) => (
+          <div className='container-2' key={index}>
+            <h2>
+              {index + 1} - {val.username}
+              <i><small>{val.points}</small></i>
+            </h2>
+          </div>
+        ))
+      }
+    </>
+  )
+}
+
+const sortUsers = (users) => { 
+  // sort with asending order 
+  return users.sort((a, b) => { 
+    if (a.points === b.points) { 
+      return b.points - a.points; 
+    } else { 
+      return b.points - a.points; 
+    } 
+  }) 
+} 
+
+
 const Leaderboard = () => {
   return(
-    <div>Leaderboard</div>
+    <div className="container-2 center">
+      <div className="row creepy-text-container">
+        <h1 className='creepy-text'>Leaderboard</h1> 
+        <h1 className='creepy-text'>Leaderboard</h1> 
+        <h1 className='creepy-text'>Leaderboard</h1> 
+      </div>
+      <Profiles sampleUsers={sortUsers(sampleUsers)}/>
+    </div>
   )
 }
 
